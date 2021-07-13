c41907ac8b9c77d24b8cf0bba76031b00645998f
riyad <hifazakhatun1108@gmail.com>
2021-07-13 15:27:42 +0600

    context text added

---
+++ java-script-context.js
@@ -0,0 +1,34 @@
+/* What is an Execution Context?
+Simply put, an execution context is an abstract
+concept of an environment where the Javascript 
+code is evaluated and executed.
+Whenever any code is run in JavaScript,
+it’s run inside an execution context  */
+
+
+/*
+Types of Execution Context
+
+1. Global Execution Context.
+2.Global Execution Context
+3.Global Execution Context
+
+*/
+
+
+/*
+How is the Execution Context created?
+
+The Creation Phase
+   1. LexicalEnvironment
+        Environment Record
+          Declarative environment record
+          Object environment record
+        Reference to the outer environment,
+        This binding.
+
+    
+  2.VariableEnvironment
+Execution Phase
+
+*/
